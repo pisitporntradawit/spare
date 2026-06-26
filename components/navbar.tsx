8ea030efeed14abf428a7b68cf271804175f79fc
pisitporntradawit <pisit.porntradawit@gmail.com>
2026-06-26 21:11:01 +0700

    add navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+
+export default function Navbar() {
+
+  return (
+    <nav className="flex items-center justify-between p-5 border-b bg-gray-800">
+
+      <div className="font-bold text-xl">
+        My App
+      </div>
+      <div className="flex gap-5">
+        <Link href="/">
+          Home
+        </Link>
+        <Link href="/role">
+          Page 2
+        </Link>
+        <Link href="/users">
+          Users
+        </Link>
+      </div>
+    </nav>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "../../components/navbar";
 
 export const metadata: Metadata = {
   title: "test",
@@ -23,10 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en">
+      <Navbar />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
